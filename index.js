d8be9fce222e50b5cddd2b99d001fb58715fa1e6
utkarshsahu101 <utkarshsahu101@gmail.com>
2020-09-25 07:22:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+// const { admin } = require("firebase-admin/lib/database");
+const functions = require("firebase-functions");
+const app = require("express")();
+const FBAuth = require("./util/fbAuth");
+const db = require("./util/admin");
+
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream,
+} = require("./handlers/screams");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+} = require("./handlers/users");
+
+// const firebase = require("firebase");
+// firebase.initializeApp(firebaseConfig);
+
+// Scream routes
+app.get("/screams", getAllScreams);
+app.post("/scream", FBAuth, postOneScream);
+app.get("/scream/:screamId", getScream);
+app.delete("/scream/:screamId", FBAuth, deleteScream);
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
+app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
+
+// Users route
+app.post("/signup", signup);
+app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
+
+exports.api = functions.region("asia-south1").https.onRequest(app);
+
+exports.createNotificationLike = functions
+  .region("asia-south1")
+  .firestore.document("likes/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnLike = functions
+  .region("asia-south1")
+  .firestore.document("comments/{id}")
+  .onDelete((snapshot) => {
+    db.doc(`/notificaions/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions
+  .region("asia-south1")
+  .firestore.document("comments/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
---
+++ util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyBnAA-vznNxDqDirXhLX45uyHesF8Qw59o",
+  authDomain: "socialape-14722.firebaseapp.com",
+  databaseURL: "https://socialape-14722.firebaseio.com",
+  projectId: "socialape-14722",
+  storageBucket: "socialape-14722.appspot.com",
+  messagingSenderId: "252489984490",
+  appId: "1:252489984490:web:ec4584e4d1b40552df2a93",
+  measurementId: "G-Q1309037S9",
+};
